450c355487e91ddfb735ecb6fb932a0e69e86453
ShubhamPrakash <d1901.bca@gmail.com>
2022-02-27 18:12:55 +0530

    useState testing

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React from "react";
+import memeData from "../memeData";
+
 
 export default function Meme() {
-  return <div className='meme-container'>
-      <form className='form' action="">
-          <input className='meme-text' type="top-text" placeholder='Top Content'/>
-          <input className='meme-text'  type="bot-text" placeholder='Bottom Content' />
-          <button className='btn'>Get a new meme image 🖼</button>
+
+    const [memeImg,setmemeImg] = React.useEffect("");
+
+
+    function getMemeImg(){
+      // console.log("testingMemeBtn");
+
+      const memesArray = memeData.data.memes;
+      const randomNum = Math.floor(Math.random()*memesArray.length)
+      // console.log(randomNum);
+      setmemeImg(memesArray[randomNum].url)
+
+
+    }
+
+  return (
+    <main>
+    <div className="meme-container">
+      <form className="form" action="">
+
+        <input
+          className="meme-text"
+          type="top-text"
+          placeholder="Top Content"
+        />
+
+
+        <input
+          className="meme-text"
+          type="bot-text"
+          placeholder="Bottom Content"
+        />
+
+
+        <button className="btn"
+          onClick={getMemeImg}>
+          Get a new meme image 🖼</button>
       </form>
-  </div>;
+
+    </div>
+    <img src={memeImg} alt="" />
+    </main>
+  );
 }
